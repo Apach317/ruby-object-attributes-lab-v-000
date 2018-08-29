9b6320d81b643c928e1a75dbcb77ae56af84eb3d
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-29 04:03:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,4 @@ class Dog
     @name = name
     @breed = breed
   end
-  def name
-    @name
-  end
-  def name
-    @name = dog_name
-  end
-  def breed
-    @breed
-  end
-  def breed
-    @breed = breed_type
 end
