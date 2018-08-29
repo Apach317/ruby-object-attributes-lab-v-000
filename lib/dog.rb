f0aff0d02e5c1c826e3382685974b1aa44aadcb1
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-29 04:10:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,11 +7,13 @@ class Dog
     @name
   end
   def name= (dog_name)
-    @name = dog_name
+    @name = dog_name.strip
   end
   def breed
     @breed
   end
   def breed= (breed_type)
-    @breed = breed_type
+    @breed = breed_type.strip
+  end
+
 end
